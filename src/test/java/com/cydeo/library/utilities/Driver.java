b7e18ff85d843894c7d592bfd2b84cbf6d76b8d5
Enes Aybars Aydın <aybarsaybars@gmail.com>
2022-11-09 01:12:42 +0300

    updated ChromeDriver language

--- src/test/java/com/cydeo/library/utilities/Driver.java
+++ src/test/java/com/cydeo/library/utilities/Driver.java
@@ -3,6 +3,7 @@ package com.cydeo.library.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -16,8 +17,33 @@ public class Driver {
     private static WebDriver driver;
 
 
+    /* To change the language of the webDriver
+
+    // Could not find the Chromedriver.exe path. Check this out!
+
+        System.setProperty("webdriver.chrome.driver","<PATH>/chromedriver.exe");
+
+        System.setProperty("webdriver.chrome.driver", "C:\Users\ghs6kor\Desktop\Java\chromedriver.exe");
+
+    //ChromeOptions object
+    ChromeOptions opt = new ChromeOptions();
+
+    //set language to Spanish
+        opt.addArguments("−−lang=es");
+
+    // configure options parameter to Chrome driver
+    WebDriver driver = new ChromeDriver(opt);
+        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+        driver.get("https://www.google.com/ ");
+
+         */
+
 
     public static WebDriver getDriver(){
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ABRA\\.cache\\selenium\\chromedriver\\win32\\107.0.5304.62\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--lang=en");
 
         if(driver == null){
 
@@ -27,7 +53,7 @@ public class Driver {
 
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
+                    driver = new ChromeDriver(options);
                     driver.manage().window().maximize();
                     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
                     break;
